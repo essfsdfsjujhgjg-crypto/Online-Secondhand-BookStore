1200b588fc08900a322911333ce3899d85be31fd
essfsdfsjujhgjg-crypto <essfsdfsjujhgjg@gmail.com>
2026-04-14 19:36:25 +0800

    Add files via upload

---
+++ user/wrong.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wrong Credentials</title>
+   
+</head>
+<body>
+    <h1>Invalid Credentials</h1>
+    <p>The username or password you entered is incorrect. Please try again.</p>
+   
+</body>
+</html>
